8ab3e9dda896a7c40a02dcfc978e88ad898ad776
31427782 <31427782@087846-P33S105.macklabs.br>
2015-09-23 10:22:11 -0300

    criação de uma mensagem de erro para PapelMoedaException

--- Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
+++ Source Code Inspection/src/br/calebe/ticketmachine/exception/PapelMoedaInvalidaException.java
@@ -4,6 +4,8 @@ package br.calebe.ticketmachine.exception;
  *
  * @author Calebe de Paula Bianchini
  */
-public class PapelMoedaInvalidaException extends Exception {
-    
+public class PapelMoedaInvalidaException extends Exception { 
+    public void msg(){
+        System.out.println("Papel moeda inválido");
+    }
 }
